458546a56bd3643129538a252d4c9e45fb8d21e3
Joseph Brown <joseph.d.brown94@gmail.com>
2020-04-15 14:49:52 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0; int num2 = 0;
+
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            Console.WriteLine("Type a number, and then press enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type another number, and then press enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
+
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    break;
+
+                case "b":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+
+                case "c":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    break;
+
+                case "d":
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+
+            }
+
+            Console.WriteLine("Press any key to close the Calculator Console App...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# C# Calculator Console App
+## Author - Oejay94
+
+This is my very first **C# App** and I'm pretty excited about it. It is a very simple calculator app that runs in the terminal. While it may not be anything super fancy or extravagant, I believe that this is a great first app for beginners of **C#**. The source for helping me better understand the basics of **C#** can be found at this [Microsoft Page](https://docs.microsoft.com/en-us/visualstudio/get-started/csharp/tutorial-console?view=vs-2019). Since the walkthrough shown in the link was written for Visual Studio and I used Visual Studio Code, I used this [other Microsoft Page](https://docs.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code) to help get started. 
+
+In order for people using Visual Studio Code to run the application they need to fork and clone this repo and then open up their VSCode terminal and type 
+
+                                    dotnet run
+Once the user has typed that and hit 'enter', the terminal will run the application and the user can then input two numbers and decide wether to add, subtract, multiply, or divide those two numbers. The terminal will then display the results. 
+
+Like I said before, it is not a complex application by any means but it is a good first start. I plan to invest more time into learning **C#** and will soon be able build bigger and better projects.
